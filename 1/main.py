90b8a77214a41be17d63abaf46a60b7640e424f0
Dean Rumsby <dean@deanrumsby.com>
2022-06-25 20:07:35 +0100

    add solution for 1

---
+++ 1/main.py
@@ -0,0 +1,10 @@
+def multiples_of_3_or_5(n: int):
+    """Finds the sum of all the multiples of 3 and 5 below n"""
+    total = 0
+    for m in range(n):
+        if (m % 3 == 0) or (m % 5 == 0):
+            total += m
+    return total
+
+
+print(multiples_of_3_or_5(1000))
