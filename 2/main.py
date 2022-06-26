e648a9119934affdcd16e2c6360445460ef958ce
Dean Rumsby <dean@deanrumsby.com>
2022-06-26 10:14:18 +0100

    add solution for 2

---
+++ 2/main.py
@@ -0,0 +1,15 @@
+def even_fibonacci_numbers(n):
+    """Finds the sum of the even fibonacci numbers with values less than n"""
+    fib_a, fib_b = 0, 1
+    temp = 0
+    total = 0
+    while fib_b < n:
+        if fib_b % 2 == 0:
+            total += fib_b
+        temp = fib_b
+        fib_b += fib_a
+        fib_a = temp
+    return total
+
+
+print(even_fibonacci_numbers(4000000))
