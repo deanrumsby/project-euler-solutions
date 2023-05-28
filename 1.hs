68862d5860ed9e57d21fc17ed619ca1c4eb84f19
Dean Rumsby <dean@deanrumsby.com>
2023-05-28 12:39:26 +0100

    refactor: remove unecessary 'do' from main in 1 and 2

--- 1.hs
+++ 1.hs
@@ -3,5 +3,4 @@
 multiplesOf3and5 = [x | x <- [1..], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 main :: IO ()
-main = do
-        print $ sum $ takeWhile (< 1000) multiplesOf3and5
+main = print $ sum $ takeWhile (< 1000) multiplesOf3and5
--- 2.hs
+++ 2.hs
@@ -3,5 +3,4 @@
 fibonacci = 1 : 2 : zipWith (+) fibonacci (tail fibonacci)
 
 main :: IO ()
-main = do
-        print $ sum $ filter even $ takeWhile (< 4000000) fibonacci
+main = print $ sum $ filter even $ takeWhile (< 4000000) fibonacci
